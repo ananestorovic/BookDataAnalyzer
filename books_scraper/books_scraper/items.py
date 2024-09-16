0f4ae4ab506bd4917adc362a89291d3e8969aed1
ananestorovic <ananestorovic28@gmail.com>
2024-09-16 19:33:32 +0200

    added PostgreSQL database

--- books_scraper/books_scraper/items.py
+++ books_scraper/books_scraper/items.py
@@ -7,13 +7,13 @@ import scrapy
 
 class BooksScraperItem(scrapy.Item):
     code = scrapy.Field()           # Šifra proizvoda
-    description = scrapy.Field()    # Opis knjige
     title = scrapy.Field()          # Naslov knjige
-    price = scrapy.Field()          # Cena knjige
     author = scrapy.Field()         # Autor knjige
+    price = scrapy.Field()          # Cena knjige
     category = scrapy.Field()       # Kategorija knjige
     publisher = scrapy.Field()      # Izdavač knjige
     binding = scrapy.Field()        # Povez knjige
     year = scrapy.Field()           # Godina izdavanja
     format = scrapy.Field()         # Format knjige
     pages = scrapy.Field()          # Broj stranica
+    description = scrapy.Field()    # Opis knjige
--- books_scraper/books_scraper/pipelines.py
+++ books_scraper/books_scraper/pipelines.py
@@ -5,9 +5,48 @@
 
 
 # useful for handling different item types with a single interface
+
 from itemadapter import ItemAdapter
+import psycopg2
+from scrapy.exceptions import DropItem
 
 
 class BooksScraperPipeline:
+
+    def open_spider(self, spider):
+        self.connection = psycopg2.connect(
+            host='localhost',
+            user='postgres',
+            password='postgres',
+            database='books_database'
+        )
+        self.cursor = self.connection.cursor()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connection.close()
+
     def process_item(self, item, spider):
+        try:
+            self.cursor.execute("""
+                        INSERT INTO books (code, title, author, price, category, publisher, binding, year, format, pages, description) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """, (
+                item.get('code'),
+                item.get('title'),
+                item.get('author'),
+                item.get('price'),
+                item.get('category'),
+                item.get('publisher'),
+                item.get('binding'),
+                item.get('year'),
+                item.get('format'),
+                item.get('pages'),
+                item.get('description')
+            ))
+            self.connection.commit()
+        except psycopg2.Error as e:
+            spider.logger.error(f"Error: {e}")
+            raise DropItem(f"Error inserting item: {e}")
+
         return item
